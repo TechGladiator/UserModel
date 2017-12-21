cfb91345b8d1938a501965a3554e29be806883b6
John Stratton <jstrat0407@gmail.com>
2017-12-20 21:55:32 -0500

    created variables for user data

--- app/src/main/java/gladiator/tech/usermodel/model/userData.java
+++ app/src/main/java/gladiator/tech/usermodel/model/userData.java
@@ -1,4 +1,9 @@
 package gladiator.tech.usermodel.model;
 
 public class userData {
+    private String username;
+    private String realName;
+    private int age;
+    private String gender;
+    private String location;
 }
