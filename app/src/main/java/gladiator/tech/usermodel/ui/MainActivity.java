4494f5470722a5cc1400f400889a231dbd301397
John Stratton <jstrat0407@gmail.com>
2017-12-20 22:43:14 -0500

    added views

--- app/src/main/java/gladiator/tech/usermodel/ui/MainActivity.java
+++ app/src/main/java/gladiator/tech/usermodel/ui/MainActivity.java
@@ -2,14 +2,30 @@ package gladiator.tech.usermodel.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import gladiator.tech.usermodel.R;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText username;
+    private EditText realName;
+    private EditText age;
+    private EditText gender;
+    private EditText location;
+    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        realName = findViewById(R.id.realName);
+        age = findViewById(R.id.age);
+        gender = findViewById(R.id.gender);
+        location = findViewById(R.id.location);
+        submitButton = findViewById(R.id.submitButton);
+
     }
 }
