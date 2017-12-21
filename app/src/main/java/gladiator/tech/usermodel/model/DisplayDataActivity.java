6123b2c048d1eb9cb5d5d9682b7558b55f37c3bd
John Stratton <jstrat0407@gmail.com>
2017-12-21 12:23:43 -0500

    added DisplayDataActivity

---
+++ app/src/main/java/gladiator/tech/usermodel/model/DisplayDataActivity.java
@@ -0,0 +1,15 @@
+package gladiator.tech.usermodel.model;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import gladiator.tech.usermodel.R;
+
+public class DisplayDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_data);
+    }
+}
