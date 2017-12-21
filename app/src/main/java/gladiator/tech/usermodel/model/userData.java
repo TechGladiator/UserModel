77117125e836bc8d7a8fa2bd3e3ca1d9323e309b
John Stratton <jstrat0407@gmail.com>
2017-12-20 21:57:06 -0500

    created getters and setters for user data variables

--- app/src/main/java/gladiator/tech/usermodel/model/userData.java
+++ app/src/main/java/gladiator/tech/usermodel/model/userData.java
@@ -6,4 +6,44 @@ public class userData {
     private int age;
     private String gender;
     private String location;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
