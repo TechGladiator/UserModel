b205d8683679a849e1181e9a7a6472f0bb5a539c
John Stratton <jstrat0407@gmail.com>
2017-12-20 21:24:09 -0500

    moved MainActivity into ui

--- app/src/main/java/gladiator/tech/usermodel/ui/MainActivity.java
+++ app/src/main/java/gladiator/tech/usermodel/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package gladiator.tech.usermodel;
+package gladiator.tech.usermodel.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import gladiator.tech.usermodel.R;
 
 public class MainActivity extends AppCompatActivity {
 
