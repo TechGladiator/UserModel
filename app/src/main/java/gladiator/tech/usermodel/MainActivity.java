79e14956e9b9cf5c10498593b7f803bb5c454102
John Stratton <jstrat0407@gmail.com>
2017-12-20 21:19:55 -0500

    created project

---
+++ app/src/main/java/gladiator/tech/usermodel/MainActivity.java
@@ -0,0 +1,13 @@
+package gladiator.tech.usermodel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
