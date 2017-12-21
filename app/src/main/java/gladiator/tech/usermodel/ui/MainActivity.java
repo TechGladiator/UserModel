5c0f6297ac3e5baecc6ecfa747b0db4f1cbff460
John Stratton <jstrat0407@gmail.com>
2017-12-21 12:44:16 -0500

    created intent

--- app/src/main/java/gladiator/tech/usermodel/ui/MainActivity.java
+++ app/src/main/java/gladiator/tech/usermodel/ui/MainActivity.java
@@ -1,5 +1,7 @@
 package gladiator.tech.usermodel.ui;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import gladiator.tech.usermodel.R;
+import gladiator.tech.usermodel.model.DisplayDataActivity;
 
 public class MainActivity extends AppCompatActivity {
     private EditText mUsername;
@@ -14,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     private EditText mAge;
     private EditText mGender;
     private EditText mLocation;
-    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +28,30 @@ public class MainActivity extends AppCompatActivity {
         mAge = findViewById(R.id.age);
         mGender = findViewById(R.id.gender);
         mLocation = findViewById(R.id.location);
-        submitButton = findViewById(R.id.submitButton);
+        Button submitButton = findViewById(R.id.submitButton);
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String username = mUsername.getText().toString();
                 String realName = mRealName.getText().toString();
-                int age = Integer.parseInt(mAge.getText().toString());
+                String age = mAge.getText().toString();
                 String gender = mGender.getText().toString();
                 String location = mLocation.getText().toString();
+
+                String[] userInfo = { username, realName, age, gender, location };
+
+                showData(userInfo);
             }
         });
 
+    }
+
+    private void showData(String[] userInfo) {
+        Intent intent = new Intent(this, DisplayDataActivity.class);
+        Resources resources = getResources();
+        String key = getString(R.string.key_name);
+        intent.putExtra(key, userInfo);
+        startActivity(intent);
     }
 }
